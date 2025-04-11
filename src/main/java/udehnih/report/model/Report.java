487da0ce4475af9fdf466dbb694d2f97665a9ade
sdikyarts <wtrcrss.family@gmail.com>
2025-04-11 10:36:37 +0700

    Feature: Modify Report model

--- src/main/java/udehnih/report/model/Report.java
+++ src/main/java/udehnih/report/model/Report.java
@@ -5,21 +5,36 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import org.hibernate.annotations.CreationTimestamp;
 import java.time.LocalDateTime;
-import java.util.UUID;
 
-
+@Entity
 @Table(name = "report")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class Report {
-    private UUID reportId;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer reportId;
+
+    @Column(name = "student_id", nullable = false)
     private String studentId;
+
+    @Column(nullable = false)
     private String title;
+
+    @Column(nullable = false)
     private String detail;
+
+    @Column(nullable = false)
     private String status;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
     private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
     private LocalDateTime updatedAt;
 }
