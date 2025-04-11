17fa564cba4f92bb5e52d5a942104d58f26187ab
sdikyarts <wtrcrss.family@gmail.com>
2025-04-11 10:38:05 +0700

    Feature: Modify Report model

--- src/main/java/udehnih/report/model/Report.java
+++ src/main/java/udehnih/report/model/Report.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+
 import org.hibernate.annotations.CreationTimestamp;
 import java.time.LocalDateTime;
 
