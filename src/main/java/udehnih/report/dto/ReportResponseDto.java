f61edb5f403b6acbfe0e1667e24cad7245b41cf3
sdikyarts <wtrcrss.family@gmail.com>
2025-05-21 12:40:34 +0700

    [REFACTOR] Modify Report Response DTO

--- src/main/java/udehnih/report/dto/ReportResponseDto.java
+++ src/main/java/udehnih/report/dto/ReportResponseDto.java
@@ -1,6 +1,7 @@
 package udehnih.report.dto;
 
 import lombok.Data;
+import udehnih.report.enums.ReportStatus;
 import java.time.LocalDateTime;
 
 @Data
@@ -9,7 +10,7 @@ public class ReportResponseDto {
     private String studentId;
     private String title;
     private String detail;
-    private String status;
+    private ReportStatus status;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 } 
