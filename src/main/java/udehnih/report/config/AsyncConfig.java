d27b665b51e7c4ab83b81a1ce907df7c65ce11e3
sdikyarts <wtrcrss.family@gmail.com>
2025-05-24 18:18:35 +0700

    feat: fix async after JWT implementation

--- src/main/java/udehnih/report/config/AsyncConfig.java
+++ src/main/java/udehnih/report/config/AsyncConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.security.task.DelegatingSecurityContextAsyncTaskExecutor;
 import java.util.concurrent.Executor;
 
 @Configuration
@@ -18,6 +19,6 @@ public class AsyncConfig {
         executor.setQueueCapacity(100);
         executor.setThreadNamePrefix("ReportThread-");
         executor.initialize();
-        return executor;
+        return new DelegatingSecurityContextAsyncTaskExecutor(executor);
     }
 } 
